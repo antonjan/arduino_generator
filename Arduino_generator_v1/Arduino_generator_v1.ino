568bf6806fe03da13dc27ea89b4c854e2fa1e9d4
Anton Janovsky <anton.janovsky@gmail.com>
2019-09-18 14:57:42 +0200

    generator start Stop

---
+++ Arduino_generator_v1/Arduino_generator_v1.ino
@@ -0,0 +1,102 @@
+//#include <Adafruit_ESP8266.h>
+
+/*
+ * ESP8266 NodeMCU LED Control over WiFi Demo
+ *
+ * https://circuits4you.com
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+ 
+//ESP Web Server Library to host a web page
+#include <ESP8266WebServer.h>
+ 
+//---------------------------------------------------------------
+//Our HTML webpage contents in program memory
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi Generator Admin: </h1><br>
+Ciclk to turn <a href="generatorOn" target="myIframe">Generator ON</a><br>
+Ciclk to turn <a href="generatorOff" target="myIframe">Generator OFF</a><br>
+Generator State:<iframe name="myIframe" width="100" height="25" frameBorder="0"><br>
+<hr>ESP8266WiFi
+<a href="http://www.giga.co.za">Giga Technology</a>
+</center>
+ 
+</body>https://circuits4you.com/handleRoot
+</html>
+)=====";
+//---------------------------------------------------------------
+//On board LED Connected to GPIO2
+#define LED 2  
+ 
+//SSID and Password of your WiFi router
+const char* ssid = "ZTE_24F54B";
+const char* password = "91275731";
+ 
+//Declare a global object variable from the ESP8266WebServer class.
+ESP8266WebServer server(80); //Server on port 80
+ 
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ Serial.println("You called root page");
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+ 
+void handleLEDon() { 
+ Serial.println("Generator on page");
+ digitalWrite(LED,LOW); //LED is connected in reverse
+ server.send(200, "text/html", "ON"); //Send ADC value only to client ajax request
+}
+ 
+void handleLEDoff() { 
+ Serial.println("LED off page");
+ digitalWrite(LED,HIGH); //LED off
+ server.send(200, "text/html", "OFF"); //Send ADC value only to client ajax request
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void){
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("Connecting to router");
+ 
+  //Onboard LED port Direction output
+  pinMode(LED,OUTPUT); 
+  //Power on LED state off
+  digitalWrite(LED,HIGH);
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+ 
+  server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+  server.on("/generatorOn", handleLEDon); //as Per  <a href="generatorOn">, Subroutine to be called
+  server.on("/generatorOff", handleLEDoff);
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void){
+  server.handleClient();          //Handle client requests
+}
